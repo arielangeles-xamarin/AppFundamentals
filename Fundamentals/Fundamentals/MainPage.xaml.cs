fb827c8f1d059a0155a98cd5ecfb8d167e942170
arielangeles <arielangeles123@hotmail.com>
2020-12-24 03:16:48 -0400

    added dependency service

---
+++ Fundamentals/Fundamentals/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Fundamentals.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Fundamentals
+{
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+
+    }
+}
---
+++ Fundamentals/Fundamentals/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Fundamentals.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace Fundamentals.ViewModels
+{
+    public class MainViewModel
+    {
+        //IDeviceOrientationService orientationService;
+        public ICommand GetDeviceOrientationCommand { get; set; }
+
+        IDeviceOrientationService orientationService { get; set; }
+
+        public string OrientationLabel { get; set; }
+
+        public string DeviceOrientationTitle => "Device Orientation";
+        public string DeviceOrientationIcon => "DeviceOrientationImg";
+
+        public MainViewModel()
+        {
+            orientationService = DependencyService.Get<IDeviceOrientationService>();     
+            DeviceOrientation orientation = orientationService.GetOrientation();
+
+            GetDeviceOrientationCommand = new Command(() => GetOrientationDevice(orientation));
+        }
+
+        private void GetOrientationDevice(DeviceOrientation orientation) => OrientationLabel = orientation.ToString();
+        
+    }
+}

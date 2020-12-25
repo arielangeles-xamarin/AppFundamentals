7a73d86777f89bfd87f812ad67c24c421fb3fddf
arielangeles <arielangeles123@hotmail.com>
2020-12-25 09:34:36 -0400

    added focsu effect

---
+++ Fundamentals/Fundamentals/FocusEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Fundamentals
+{
+	public class FocusEffect : RoutingEffect
+	{
+		public FocusEffect() : base("MyCompany.FocusEffect")
+		{
+		}
+	}
+}
